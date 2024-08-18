aa3532143da22d45a9f7df4c4b932cf1ffabbcf0
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-18 23:45:33 +0600

    Create problem2.sql

---
+++ Easy/problem2.sql
@@ -0,0 +1,7 @@
+'''Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically. '''
+SELECT 
+  CITY, LENGTH(CITY) AS name_length 
+  FROM STATION 
+  ORDER BY LENGTH(CITY) ASC, 
+  CITY ASC 
+  LIMIT 1;
