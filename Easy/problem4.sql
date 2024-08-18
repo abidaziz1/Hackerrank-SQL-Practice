7cc96a24b4e2328ca100cba90e1c2f2a0580406e
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-18 23:52:35 +0600

    Update problem4.sql

--- Easy/problem4.sql
+++ Easy/problem4.sql
@@ -1,6 +1,7 @@
 """Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates. """
 select city
+  from station
   WHERE 
   UPPER(SUBSTR(CITY, 1, 1)) IN ('A', 'E', 'I', 'O', 'U') 
   AND UPPER(SUBSTR(CITY, -1)) IN ('A', 'E', 'I', 'O', 'U');
-from station;
+
