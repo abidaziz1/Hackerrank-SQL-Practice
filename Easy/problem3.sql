5616cfbd3691198722a8828fa564773e8b7009c5
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-18 23:47:11 +0600

    Create problem3.sql

---
+++ Easy/problem3.sql
@@ -0,0 +1,4 @@
+"""Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates."""
+
+"""A hint below how to solve this problem"""
+Where city ILIKE ‘a%’ or city ILIKE ‘’....
