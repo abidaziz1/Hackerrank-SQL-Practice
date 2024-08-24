550869bfe42a4896244ba6a4df42c9f90c4cf923
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-24 19:03:45 +0600

    Create Problem24.sql

---
+++ Easy/Problem24.sql
@@ -0,0 +1,11 @@
+/*
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY NOT LIKE '%A' 
+AND CITY NOT LIKE '%E' 
+AND CITY NOT LIKE '%I' 
+AND CITY NOT LIKE '%O' 
+AND CITY NOT LIKE '%U';
