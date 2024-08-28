1b43902b12d78e7ca6fe205a1f09cd6fd04363d7
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-29 00:02:58 +0600

    Update JOIN1.sql

--- Easy/JOIN1.sql
+++ Easy/JOIN1.sql
@@ -1,12 +1,12 @@
 /*
 Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
-
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 */
 select city.name
 from city
 join country on city.countrycode=country.code
-where country.continent='Africa';
+where 
+  country.continent='Africa'; 
 /*
 Tables Overview:
 CITY Table:
